16bbfb5d727a56bb152ff9f491de835d10f5e2b2
kaanalgan <34076340+kaanalgan@users.noreply.github.com>
2018-11-04 21:51:52 +0300

    Update PercussionInstrumentMusician.java

--- hw2/src/Business/PercussionInstrumentMusician.java
+++ hw2/src/Business/PercussionInstrumentMusician.java
@@ -1,5 +1,35 @@
 package Business;
 
 public class PercussionInstrumentMusician extends Musician {
-
+	
+	
+	public PercussionInstrumentMusician() {
+		super();
+	}
+	
+	public PercussionInstrumentMusician(Tempo tempo) {
+		super(tempo);
+	}
+	
+	PercussionInstrumentMusician(PercussionInstrumentMusician pim){
+		this(pim.getTempo());
+	}
+	
+	@Override
+	public String playPiece(Piece piece) {
+		checkIfNull(piece);
+		String whatWillBePlayed = "";
+		String[] parts = piece.getParts();
+		String lastPartToPlay = parts[parts.length-1];
+		String firstPartToPlay = parts[parts.length-2];
+		whatWillBePlayed = whatWillBePlayed + firstPartToPlay;
+		whatWillBePlayed = whatWillBePlayed + lastPartToPlay;
+		return whatWillBePlayed;
+	}
+	
+	private void checkIfNull(Object object) {
+		if(object == null) {
+			throw new IllegalArgumentException("Given object is null, therefore the program cannot play the song.");
+		}
+	}
 }
