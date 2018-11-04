83c16fefb3812dd630d72af987331143d4962a07
kaanalgan <34076340+kaanalgan@users.noreply.github.com>
2018-11-04 21:51:50 +0300

    Update Piece.java

--- hw2/src/Business/Piece.java
+++ hw2/src/Business/Piece.java
@@ -3,26 +3,45 @@ package Business;
 import java.util.Arrays;
 
 public class Piece {
+	
 	private String[] parts;
+	private boolean isInitialized = false;
 	
 	public Piece() {
 		this.parts = new String[0];
+		isInitialized = true;
 	}
 	
 	public Piece(String piece) {
+		checkIfNull(piece);
 		this.parts = piece.split("\n");
+		isInitialized = true;
 	}
 	
 	Piece(Piece piece) {
-		String newPiece = String.join("\n", parts);
-		new Piece(newPiece);
+		this(String.join("\n", piece.getParts()));
 	}
 
 	public String[] getParts() {
+		checkInitialization();
 		return Arrays.copyOf(parts, parts.length);
 	}
 
 	public void setParts(String[] parts) {
+		checkInitialization();
+		checkIfNull(parts);
 		this.parts = parts;
+	}
+	
+	private void checkInitialization() {
+		if(!isInitialized) {
+			throw new IllegalStateException("Given piece object was not created properly.");
+		}
+	}
+	
+	private void checkIfNull(Object object) {
+		if(object == null) {
+			throw new IllegalArgumentException("Given piece argument is null, therefore cannot be played.");
+		}
 	}
 }
