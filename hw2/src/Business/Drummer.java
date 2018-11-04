aacc206fc0e7decc40ca4ab4835d5ef230dda058
kaanalgan <34076340+kaanalgan@users.noreply.github.com>
2018-11-04 21:47:52 +0300

    Update Drummer.java

--- hw2/src/Business/Drummer.java
+++ hw2/src/Business/Drummer.java
@@ -2,4 +2,50 @@ package Business;
 
 public class Drummer extends PercussionInstrumentMusician {
 
+	private boolean isInitialized = false;
+	
+	public Drummer() {
+		isInitialized = true;
+	}
+	
+	@Override
+	public String playPiece(Piece piece) {
+		checkInitialization();
+		checkIfNull(piece);
+		String whatToPlay = "X";
+		if((this.getTempo() == Tempo.prestissimo) || (this.getTempo() == Tempo.vivace) ||
+			this.getTempo() == Tempo.allegretto){
+				whatToPlay = getTheNotesToPlay(piece);
+			}
+		return whatToPlay;
+	}
+	
+	private String getTheNotesToPlay(Piece piece) {
+		checkIfNull(piece);
+		String whatDrummerPlays = "Drummer is played: \n";
+		String allPartsOfThePiece = super.playPiece(piece);
+		String[] allPartsArray = allPartsOfThePiece.split("\n");
+		for(String note : allPartsArray) {
+			if((note.charAt(0) == 'C') || (note.charAt(0) == 'D') || (note.charAt(0) == 'E')) {
+				whatDrummerPlays += " " + note.charAt(0);
+			}
+			else {
+				whatDrummerPlays += " X";
+			}
+		}
+		return whatDrummerPlays + " " + this.getTempo().toString();
+	}
+	
+	private void checkInitialization() {
+		if(!isInitialized) {
+			throw new IllegalStateException("Given drummer object was not created properly.");
+			
+		}
+	}
+	
+	private void checkIfNull(Piece piece) {
+		if(piece == null) {
+			throw new IllegalArgumentException("Given piece argument is null, therefore cannot be played.");
+		}
+	}
 }
