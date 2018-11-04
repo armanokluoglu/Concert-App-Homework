87bdbc987e672cc1b208e4ec7f35a35ca25bf2e1
kaanalgan <34076340+kaanalgan@users.noreply.github.com>
2018-11-04 21:51:44 +0300

    Update WoodwindInstrumentMusician.java

--- hw2/src/Business/WoodwindInstrumentMusician.java
+++ hw2/src/Business/WoodwindInstrumentMusician.java
@@ -2,4 +2,27 @@ package Business;
 
 public class WoodwindInstrumentMusician extends Musician {
 
+	public WoodwindInstrumentMusician() {
+		super();
+	}
+	
+	public WoodwindInstrumentMusician(Tempo tempo) {
+		super(tempo);
+	}
+	
+	WoodwindInstrumentMusician(WoodwindInstrumentMusician wim){
+		this(wim.getTempo());
+	}
+	
+	@Override
+	public String playPiece(Piece piece) {
+		String whatWillBePlayed = "";
+		String[] parts = piece.getParts();
+		whatWillBePlayed = getChorusNotes(piece);
+		return whatWillBePlayed;
+	}
+	
+	private String getChorusNotes(Piece piece) {
+		String chorus = "";
+	}
 }
