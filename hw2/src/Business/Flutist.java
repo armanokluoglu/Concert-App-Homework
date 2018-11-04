c7f1123d1d3a8222ac664487ebb8e93f71b1e607
kaanalgan <34076340+kaanalgan@users.noreply.github.com>
2018-11-04 21:48:27 +0300

    Update Flutist.java

--- hw2/src/Business/Flutist.java
+++ hw2/src/Business/Flutist.java
@@ -2,4 +2,21 @@ package Business;
 
 public class Flutist extends WoodwindInstrumentMusician {
 
+	private boolean isInitialized = false;
+	
+	public Flutist() {
+		isInitialized = true;
+	}
+	
+	@Override
+	public String playPiece(Piece piece) {
+		String whatWillBePlayed = "Flut is played: \n";
+		
+	}
+	
+	private void checkInitialization() {
+		if(!isInitialized) {
+			throw new IllegalArgumentException("Given flutist object was not created properly.");
+		}
+	}
 }
