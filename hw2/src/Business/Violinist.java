166f7a72faaf458d92d3eec93eb32efef2d19e4d
kaanalgan <34076340+kaanalgan@users.noreply.github.com>
2018-11-04 21:51:47 +0300

    Update Violinist.java

--- hw2/src/Business/Violinist.java
+++ hw2/src/Business/Violinist.java
@@ -2,4 +2,25 @@ package Business;
 
 public class Violinist extends StringInstrumentMusician {
 
+	private boolean isInitialized = false;
+	
+	public Violinist() {
+		isInitialized = true;
+	}
+	
+	@Override
+	public String playPiece(Piece piece) {
+		checkInitialization();
+		String whatWillBePlayed = "Violin is played: \n";
+		if(this.getTempo() != Tempo.prestissimo) {
+			whatWillBePlayed = super.playPiece(piece);
+		}
+		return whatWillBePlayed + " " + this.getTempo().toString();
+	}
+	
+	private void checkInitialization() {
+		if(!isInitialized) {
+			throw new IllegalStateException("Given violinist object was not created proeprly.");
+		}
+	}
 }
